448fee8efd42c4a7234e2c1ddebe85073b03f9f4
ila36IX <jbariali002@gmail.com>
2023-07-17 18:47:49 -0700

    Advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdoi.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
