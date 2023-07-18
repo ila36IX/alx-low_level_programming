d515152467cc56a51ef6a0dfb79f52a831653d3b
ila36IX <jbariali002@gmail.com>
2023-07-18 15:04:31 -0700

    Naming is not easy

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry function
+ *
+ * return: random number(Positive or negative)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n==0)
+	{
+		state="is zero"
+	}
+	else
+
+	return (0);
+}
