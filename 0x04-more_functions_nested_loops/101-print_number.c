0fa234976c30d0da8208bac0cf5f7dc1b23fdc2d
ila36IX <jbariali002@gmail.com>
2023-07-20 16:23:27 -0700

    Print a number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,24 +9,28 @@
  */
 void print_number(int n)
 {
+	int i, number;
+	int counter = 1;
+	int divisor = 1;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n > 1000)
+
+	number = n;
+
+	while (number / 10 > 0)
 	{
-		_putchar('0' + (int) n / 1000);
-		n = n - 1000 * (n / 1000);
+		counter++;
+		divisor *= 10;
+		number = number / 10;
 	}
-	if (n > 100)
+	for (i = 1; i <= counter; i++)
 	{
-		_putchar('0' + (int) n / 100);
-		n = n - 100 * (n / 100);
+		_putchar('0' + n / divisor);
+		n = n - (n / divisor) * divisor;
+		divisor /= 10;
 	}
-	if (n > 10)
-	{
-		_putchar('0' + (int) n / 10);
-	}
-	_putchar('0' + n % 10);
 }
