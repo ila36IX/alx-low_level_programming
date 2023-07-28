93b9eb4c4efadcae62c0b6f73eb9f6a19726cb92
ila36IX <jbariali002@gmail.com>
2023-07-27 19:02:43 -0700

    Add 101-print_number.c file

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - Print a number using _putchar
+ * @n: integer
+ *
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	int m = n;
+	int digits = 0, j;
+	int divider = 1;
+
+	if (!m)
+	{
+		_putchar('0');
+	}
+	while (m)
+	{
+		m /= 10;
+		digits++;
+		divider *= 10;
+	}
+	divider /= 10;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	for (j = 0; j < digits; j++)
+	{
+		_putchar('0' + n / divider);
+		n = n - (n / divider) * divider;
+		divider /= 10;
+	}
+}
