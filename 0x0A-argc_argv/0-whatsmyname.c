32ed77f7d0584a132f8a0c90c3acf1f44479dd3b
ila36IX <jbariali002@gmail.com>
2023-08-15 18:31:25 -0700

    Add first file of argc/argv project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Print the program name
+  *
+  * @argc - Arguments counter
+  * @argv - Argements vectory
+  * Return: Zero
+  */
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
