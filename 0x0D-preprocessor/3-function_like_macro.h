15a209063bff69d1f15c7f6f98416f5d23fe944e
ila36IX <jbariali002@gmail.com>
2023-08-20 02:31:39 +0000

    C - Preprocessor fixation

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define  _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x >= 0) ? (x) : (x * (-1)))
+#define ABS(x) ((x >= 0) ? (x) : ((x) * (-1)))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
