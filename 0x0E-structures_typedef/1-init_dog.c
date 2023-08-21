b1132296cbea664e0360062216614de4e8ecc1ca
ila36IX <jbariali002@gmail.com>
2023-08-21 05:53:32 +0000

    NULL case

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,7 +12,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (*d)
+	{
+
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
