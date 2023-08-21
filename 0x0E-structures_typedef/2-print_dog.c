6612393b3e2e34830f275dd64c5ad5ff44b40f9b
ila36IX <jbariali002@gmail.com>
2023-08-21 05:58:53 +0000

    NULL case

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,15 +15,15 @@ void print_dog(struct dog *d)
 		if (d->name)
 			printf("Name: %s\n", d->name);
 		else
-			printf("(nil)\n");
+			printf("Name: (nil)\n");
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
-			printf("(nil)\n");
+			printf("Age: (nil)\n");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 
 	}
 }
