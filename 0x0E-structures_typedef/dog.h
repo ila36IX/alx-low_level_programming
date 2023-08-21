2418adcfbf89cd94a9393ca9424dbf5d55517f74
ila36IX <jbariali002@gmail.com>
2023-08-21 05:46:04 +0000

    Betty style fixation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,9 +12,9 @@ typedef struct dog dog_t;
  */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
