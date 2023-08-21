7e78d6ac16c59e48054e927bf79ccc77fadc3795
ila36IX <jbariali002@gmail.com>
2023-08-21 07:12:32 +0000

    Fixing the file 6 style

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * is_prime_number - Checks if it prime number
+ * is_prime - Checks if it prime number
  *
  * @n: Integer
+ * @d: Integer
  * Return: 1 if it is prime and 0 otherwise
  */
 
@@ -19,6 +20,12 @@ int is_prime(int n, int d)
 		return (is_prime(n, d - 1));
 }
 
+/**
+ * is_prime_number - Checks if it prime
+ *
+ * @n: Integer
+ * Return: 1if it is prime and 0 otherwise
+ */
 int is_prime_number(int n)
 {
 		return (is_prime(n, n / 2));
