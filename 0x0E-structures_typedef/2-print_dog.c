a01a1bd000c437d04751e2d28ddcddba137d3383
ila36IX <jbariali002@gmail.com>
2023-08-21 06:08:00 +0000

    Age in the 0 cse

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,9 +16,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %f\n", d->age);
-		else
+		printf("Age: %f\n", d->age);
 			printf("Age: (nil)\n");
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
