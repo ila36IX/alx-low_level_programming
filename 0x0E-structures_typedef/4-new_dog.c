37cdc76143eae6ff2b4ff568a2ef8877cc54b90e
ila36IX <jbariali002@gmail.com>
2023-08-21 06:18:03 +0000

    String conflict fixation

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * new_dog - creates a new dog struct type
@@ -16,9 +17,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	p = &d;
 
-	d.name = name;
+	strcpy(d.name, name);
 	d.age = age;
-	d.owner = owner;
+	strcpy(d.owner, owner);
 
 	return (p);
 }
