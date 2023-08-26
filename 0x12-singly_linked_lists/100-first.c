f6b82060f430c86d2dac5e58b26ede9bd2fe7aec
ila36IX <jbariali002@gmail.com>
2023-08-26 01:42:15 +0000

    The 100th task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * startup_fun - Will be called befor main function
+ *
+ * Return: Nothing
+ */
+void startup_fun(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
