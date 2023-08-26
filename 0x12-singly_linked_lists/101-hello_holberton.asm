e7038caef721c9ffc60a013461968341b5d4e771
ila36IX <jbariali002@gmail.com>
2023-08-26 02:05:10 +0000

    Add the 101th task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+	hello db 'Hello, Holberton!',0 ;
+
+section .text
+	global main
+
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, hello
+    mov rdx, 18
+
+    syscall
+    mov rax, 60
+    xor rdi, rdi
+    syscall
