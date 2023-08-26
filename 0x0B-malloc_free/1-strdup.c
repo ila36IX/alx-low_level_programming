def109185704e3e53c264127350a460e216d9bfd
ila36IX <jbariali002@gmail.com>
2023-08-26 16:59:31 -0700

    The empty string case in file 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,13 +12,13 @@ char *_strdup(char *str)
 	char *p;
 	int i = 0;
 
-	if (str == NULL || *str == '\0')
+	if (str == NULL)
 		return (NULL);
-
+	
 	while (*(str + i))
 		i++;
 
-	p = malloc(sizeof(*str) * i);
+	p = malloc(sizeof(*str) * (i + 1));
 
 	if (!p)
 		return (NULL);
