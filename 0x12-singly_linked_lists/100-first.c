7ea735ec3d6af720d967042e220edceec1e136ae
Ali <104344010+ila36IX@users.noreply.github.com>
2023-08-27 00:30:14 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@
  */
 void startup_fun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
