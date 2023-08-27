506044f46eba576a259f76e6dfe13729681266fd
ila36IX <jbariali002@gmail.com>
2023-08-27 00:18:59 +0000

    Hondle some falcy cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,18 +13,32 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	int s1_len = 0;
 	int s2_len = 0;
-	
+
+	if (s1 == NULL)
+		return (_strdup(s2));
+	else if (s2 == NULL)
+		return (_strdup(s1));
+	else if (s1 == NULL && s2 == NULL)
+		return (_strdup(""));
+
 	while (*(s1 + s1_len))
 		s1_len++;
+
 	while (*(s2 + s2_len))
 		s2_len++;
-	p = malloc(sizeof(*s1) * (s1_len + s2_len));
-	if (!p)
+
+	p = malloc(sizeof(*s1) * (s1_len + s2_len - 1));
+
+	if (p == NULL)
 		return (NULL);
-	p[s1_len + s2_len] = '\0';
+
+	p[s1_len + s2_len - 1] = '\0';
+
 	while (s2_len--)
 		p[s1_len + s2_len] = s2[s2_len];
-	while(s1_len--)
+
+	while (s1_len--)
 		p[s1_len] = s1[s1_len];
+
 	return (p);
 }
