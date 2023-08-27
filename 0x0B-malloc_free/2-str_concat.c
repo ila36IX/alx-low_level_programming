d3e3c979c4a1dd076c69cf96cd0b3ebb5de879de
ila36IX <jbariali002@gmail.com>
2023-08-27 00:23:27 +0000

    Hondle some falcy cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - Duplicate a string
+ *
+ * @str: String
+ * Return: pointer to newly duplicate string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	
+	while (*(str + i))
+		i++;
+
+	p = malloc(sizeof(*str) * (i + 1));
+
+	if (!p)
+		return (NULL);
+
+	p[i] = '\0';
+
+	while (i--)
+		p[i] = *(str + i);
+
+	return (p);
+}
+
+/**
  * str_concat - Concatenate two strings
  *
  * @s1: First string
