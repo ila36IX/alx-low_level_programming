f7264cc8a2d7588523366df0479b20b151f77b8a
ila36IX <jbariali002@gmail.com>
2023-08-29 09:09:40 +0000

    The functions pointers task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
+	if (array == NULL)
+		return;
+
+	if (action == NULL)
+		return;
+
 	for (i = 0; i < (int) size; i++)
 	{
 		action(array[i]);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,8 +9,8 @@
  */
 int main(int argc, char **argv)
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[3]);
+	int a;
+	int b;
 	char *op = argv[2];
 	char opr = argv[2][0];
 	int (*f)(int, int);
@@ -20,7 +20,10 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (get_op_func(op) == NULL)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		return (99);
