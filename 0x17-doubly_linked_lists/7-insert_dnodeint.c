46c7227df68c2b9419cc480b7af53dce815a7976
ila36IX <jbariali002@gmail.com>
2023-10-14 03:48:44 +0000

    Fix the function insert_dnodeint_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,6 +23,21 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	return (NULL);
 }
 /**
+ * dlistint_len -  Number of elements in a linked dlistint_t list
+ *
+ * @h: Head of the list
+ * Return: Size of the DLL
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+        dlistint_t *current = (dlistint_t *) h;
+        size_t i = 0;
+
+        while (current != NULL)
+        current = current->next, i++;
+        return (i);
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  *
  * @h: Head of the DLL
@@ -33,15 +48,20 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node = get_dnodeint_at_index(*h, idx);
-	dlistint_t *new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *new_node = NULL;
+	
 
+	if (dlistint_len(*h) == idx)
+	return (add_dnodeint_end(h, n));
+	
+	new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	return (NULL);
 	if (idx == 0)
 	return (add_dnodeint(h, n));
-	if (node == NULL) 
+	if (node == NULL)
 	return (NULL);
-	
+
 	new_node->n = n;
 	new_node->next = node;
 	new_node->prev = node->prev;
@@ -49,7 +69,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (*h == NULL)
 	(*h) = new_node;
 
-	if (n != 0 && (*h) != NULL && node->prev != NULL)
+	if (n != 0 && (*h) != NULL)
 	node->prev->next = new_node;
 	else
 	(*h) = new_node;
