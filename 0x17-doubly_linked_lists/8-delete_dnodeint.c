708d63730d3dacce03ac5e41ad766b9f57650c07
ila36IX <jbariali002@gmail.com>
2023-10-14 17:47:18 +0000

    Fix the function insert_dnodeint_at_index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -36,7 +36,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *node = get_dnodeint_at_index(*head, index);
 	dlistint_t *new_head = *head;
 
-	if (head == NULL)
+	if (head == NULL && (*head) == NULL)
 	return (-1);
 
 	if (node == NULL)
