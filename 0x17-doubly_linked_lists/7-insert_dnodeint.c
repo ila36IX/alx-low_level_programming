b7145c7333c2759562a4646158839094b571c2e8
ila36IX <jbariali002@gmail.com>
2023-10-14 00:55:32 +0000

    Fix the function insert_dnodeint_at_index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,17 +9,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-        dlistint_t *current = NULL;
+	dlistint_t *current = NULL;
 
-        current = head;
-        while (current != NULL && index != 0)
-        {
-                index--;
-                current = current->next;
-        }
-        if (index == 0)
-        return (current);
-        else
+	current = head;
+	while (current != NULL && index != 0)
+	{
+		index--;
+		current = current->next;
+	}
+	if (index == 0)
+	return (current);
+	else
 	return (NULL);
 }
 
@@ -29,17 +29,22 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
  * @h: Head of the DLL
  * @idx: Index where the new node will be inserted
  * @n: THe new node date
+ * Return: Address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *node = get_dnodeint_at_index(*h, idx);
 	dlistint_t *new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
-	
+
+	if (new_node == NULL)
+	return (NULL);
 	if (node == NULL)
 	return (NULL);
+
 	new_node->n = n;
 	new_node->next = node;
 	new_node->prev = node->prev;
+	if (node->prev != NULL)
 	node->prev->next = new_node;
 
 	node->prev = new_node;
