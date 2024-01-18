7f95ce71a2e52aff7028b0dbd345202210aba4fd
ila36IX <jbariali002@gmail.com>
2024-01-18 23:54:34 +0100

    🌳 Data structure: Binary tree

--- binary_trees/1-binary_tree_insert_left.c
+++ binary_trees/1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_insert_left - inserts a node as the 
+/**
+ * binary_tree_insert_left - inserts a node as the
  * left-child of another node.
  *
  * @parent: pointer to the node to insert the left-child in
--- binary_trees/2-binary_tree_insert_right.c
+++ binary_trees/2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_insert_right - inserts a node as the 
+/**
+ * binary_tree_insert_right - inserts a node as the
  * right-child of another node.
  *
  * @parent: pointer to the node to insert the right-child in
