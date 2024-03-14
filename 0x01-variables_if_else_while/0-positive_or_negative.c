09a42a3a9d13168a0bc55b3dd1cca65bfa12f00f
ila36IX <jbariali002@gmail.com>
2024-03-14 05:58:53 +0000

    Betty I'm sorry!

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- *  * main - print all possible combinations of three digits
- *   *
- *      * Return: O if succeed
- *       **/
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - the neter point of the code
+ *
+ * Return: 0 if success
+ **/
 int main(void)
 {
 	int n;
+	int last_Digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	n = 0;
-	if (n > 0)
+	last_Digit = n % 10;
+	printf("Last digit of %d is ", n);
+
+	if (last_Digit >= 5)
 	{
-		printf("%d is positive\n", n);
+		printf("%d and is greater than 5\n", last_Digit);
 	}
-	else if (n == 0)
+	else if (last_Digit == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d and is 0\n", last_Digit);
 	}
-	else if (n < 0)
+	else if (last_Digit <= 6)
 	{
-		printf("%d is negative\n", n);
+		printf("%d and is less than 6 and not 0\n", last_Digit);
 	}
+
 	return (0);
+
 }
