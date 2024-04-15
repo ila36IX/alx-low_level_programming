884c8351acbf513f4542b9fc8cfbdfb1efce880b
ila36IX <jbariali002@gmail.com>
2024-04-15 03:42:12 +0100

    searches for a value in a sorted skip list of integers

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Searches for a value in a sorted skip list 
+ * of integers using “express lane” to browse it
+ *
+ * @list: Pointer to the head of linked list
+ * @value: value to locate its index
+ * Return: Value index if exists, if not
+ * -1 or if list is null or value not
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *e, *pe, *max;
+
+	if (!list)
+		return (NULL);
+
+	pe = list;
+	e = list->express;
+	while (e)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", e->index, e->n);
+		if (e->n > value)
+			break;
+		pe = e;
+		e = e->express;
+	}
+	if (!e)
+	{
+		e = pe;
+		while (e->next)
+			e = e->next;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", pe->index, e->index);
+	while (pe != e->next)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", pe->index, pe->n);
+		if (pe->n == value)
+			return (pe);
+		pe = pe->next;
+	}
+	return (NULL);
+}
