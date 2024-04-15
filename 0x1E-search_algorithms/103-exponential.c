1c08c111774c0cc6312dbc74f80e05a0d61726f0
ila36IX <jbariali002@gmail.com>
2024-04-15 01:37:03 +0100

    Binary search using recursion

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,6 +1,58 @@
 #include "search_algos.h"
 
 /**
+ * _searching_in_array - prints the current searching array items
+ *
+ * @array: Sorted array
+ * @l: starting position
+ * @r: Ending position
+ * Return: Nothing
+ */
+void _searching_in_array(int *array, int l, int r)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; i++)
+	{
+		printf("%d%s", array[i], i == r ? "\n" : ", ");
+	}
+}
+
+/**
+ * _binary_search - Search for an element in sorted array
+ * using binary_search
+ *
+ * @array: A sorted array
+ * @size: The size of the array
+ * @value: The value to search for
+ * Return: Index which the value is located in the array
+ * Or -1 if array if NULL or value not exists
+ */
+int _binary_search(int *array, size_t size, int value)
+{
+	int l, r, m;
+
+	if (!array)
+		return (-1);
+
+	l = 0;
+	r = size - 1;
+	while (r >= l)
+	{
+		_searching_in_array(array, l, r);
+		m = (l + r) / 2;
+		if (array[m] < value)
+			l = m + 1;
+		else if (array[m] > value)
+			r = m - 1;
+		else
+			return (m);
+	}
+	return (-1);
+}
+
+/**
  * min - find the less integer
  *
  * @d1: first integer to compare
@@ -43,60 +95,8 @@ int exponential_search(int *array, size_t size, int value)
 	}
 	end_pos = min(i, size - 1);
 	printf("Value found between indexes [%lu] and [%ld]\n", i / 2, end_pos);
-	pos = binary_search(&array[i / 2], end_pos - i / 2 + 1, value);
+	pos = _binary_search(&array[i / 2], end_pos - i / 2 + 1, value);
 	if (pos == -1)
 		return (-1);
 	return (pos + i / 2);
-}
-
-/**
- * searching_in_array - prints the current searching array items
- *
- * @array: Sorted array
- * @l: starting position
- * @r: Ending position
- * Return: Nothing
- */
-void searching_in_array(int *array, int l, int r)
-{
-	int i;
-
-	printf("Searching in array: ");
-	for (i = l; i <= r; i++)
-	{
-		printf("%d%s", array[i], i == r ? "\n" : ", ");
-	}
-}
-
-/**
- * binary_search - Search for an element in sorted array
- * using binary_search
- *
- * @array: A sorted array
- * @size: The size of the array
- * @value: The value to search for
- * Return: Index which the value is located in the array
- * Or -1 if array if NULL or value not exists
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int l, r, m;
-
-	if (!array)
-		return (-1);
-
-	l = 0;
-	r = size - 1;
-	while (r >= l)
-	{
-		searching_in_array(array, l, r);
-		m = (l + r) / 2;
-		if (array[m] < value)
-			l = m + 1;
-		else if (array[m] > value)
-			r = m - 1;
-		else
-			return (m);
-	}
-	return (-1);
 }
--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -4,14 +4,13 @@
  * recursion_binary - Implementing binary search using recursion
  *
  * @array: A sorted array
- * @size: The size of the array
  * @low: first index current search will start from
  * @high: last index current search will end by
  * @value: The value to search for
  * Return: Index which the value is located in the array
  * Or -1 if array if NULL or value not exists
  */
-int recursion_binary(int *array, size_t size, size_t low,
+int recursion_binary(int *array, size_t low,
 		     size_t high, int value)
 {
 	size_t m = (low + high) / 2;
@@ -19,17 +18,16 @@ int recursion_binary(int *array, size_t size, size_t low,
 	if (low > high)
 		return (-1);
 
+	searching_in_array(array, low, high);
 	if (array[low] == value)
 		return (low);
 
-	searching_in_array(array, low, high);
-
 	if (array[m] < value)
-		return (recursion_binary(array, size, m + 1, high, value));
+		return (recursion_binary(array, m + 1, high, value));
 	else if (array[m] > value)
-		return (recursion_binary(array, size, low, m - 1, value));
+		return (recursion_binary(array, low, m - 1, value));
 	else if (array[m - 1] == value)
-		return (recursion_binary(array, size, low, m + 1, value));
+		return (recursion_binary(array, low, m , value));
 	else
 		return (m);
 }
@@ -48,57 +46,5 @@ int advanced_binary(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	return (recursion_binary(array, size, 0, size - 1, value));
-}
-
-/**
- * searching_in_array - prints the current searching array items
- *
- * @array: Sorted array
- * @l: starting position
- * @r: Ending position
- * Return: Nothing
- */
-void searching_in_array(int *array, int l, int r)
-{
-	int i;
-
-	printf("Searching in array: ");
-	for (i = l; i <= r; i++)
-	{
-		printf("%d%s", array[i], i == r ? "\n" : ", ");
-	}
-}
-
-/**
- * binary_search - Search for an element in sorted array
- * using binary_search
- *
- * @array: A sorted array
- * @size: The size of the array
- * @value: The value to search for
- * Return: Index which the value is located in the array
- * Or -1 if array if NULL or value not exists
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int l, r, m;
-
-	if (!array)
-		return (-1);
-
-	l = 0;
-	r = size - 1;
-	while (r >= l)
-	{
-		searching_in_array(array, l, r);
-		m = (l + r) / 2;
-		if (array[m] < value)
-			l = m + 1;
-		else if (array[m] > value)
-			r = m - 1;
-		else
-			return (m);
-	}
-	return (-1);
+	return (recursion_binary(array, 0, size - 1, value));
 }
